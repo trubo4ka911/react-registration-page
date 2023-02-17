f4d2abbe77f76e6aa139424df265fa61c14ddb7e
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-18 01:52:19 +0200

    f

--- src/component/MultiStepForm/StepOne.jsx
+++ src/component/MultiStepForm/StepOne.jsx
@@ -4,67 +4,102 @@ import { bindActionCreators } from 'redux';
 import { setFormData } from '../actions/formActions';
 
 class StepOne extends Component {
-constructor(props) {
-super(props);
-this.state = {
-email: '',
-password: '',
-confirmPassword: ''
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  componentDidMount() {
+    const { formData } = this.props;
+    if (formData) {
+      const { email, password, confirmPassword } = formData;
+      this.setState({
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword
+      });
+    }
+  }
+
+  _handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _handleSubmit = event => {
+    event.preventDefault();
+    const { nextStep, setFormData } = this.props;
+    const { email, password, confirmPassword } = this.state;
+    const formData = {
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword
+    };
+    setFormData(formData);
+    nextStep();
+  };
+
+  render() {
+    const { email, password, confirmPassword } = this.state;
+    return (
+      <div>
+        <h2>Step 1</h2>
+        <form onSubmit={this._handleSubmit}>
+          <div>
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={this._handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this._handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={this._handleChange}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Next</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-_handleChange = event => {
-const { name, value } = event.target;
-this.setState({
-[name]: value
-});
-};
+const mapStateToProps = state => {
+  return {
+    formData: state.formData,
+  }
+}
 
-_handleSubmit = event => {
-event.preventDefault();
-const { nextStep, setFormData } = this.props;
-const { email, password, confirmPassword } = this.state;
-const formData = {
-email: email,
-password: password,
-confirmPassword: confirmPassword
-};
-setFormData(formData);
-nextStep();
-};
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({
+    setFormData
+  }, dispatch);
+}
 
-render() {
-const { email, password, confirmPassword } = this.state;
-return (
-<div>
-<h2>Step 1</h2>
-<form onSubmit={this._handleSubmit}>
-<div>
-<label>Email Address</label>
-<input
-           type="text"
-           name="email"
-           value={email}
-           onChange={this._handleChange}
-         />
-</div>
-<div>
-<label>Password</label>
-<input
-           type="password"
-           name="password"
-           value={password}
-           onChange={this._handleChange}
-         />
-</div>
-<div>
-<label>Confirm Password</label>
-<input
-           type="password"
-           name="confirmPassword"
-           value={confirmPassword}
-           onChange={this._handleChange}
-         />
-</div>
-<button type="submit">Next</button>
-</form>
+export default connect(mapStateToProps, mapDispatchToProps)(StepOne);
---
+++ src/component/MultiStepForm/StepTwo.jsx
@@ -0,0 +1,134 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setFormData } from '../actions/formActions';
+
+class StepTwo extends Component {
+constructor(props) {
+super(props);
+this.state = {
+firstName: '',
+lastName: '',
+address: '',
+city: '',
+state: '',
+zip: ''
+};
+}
+
+componentDidMount() {
+const { formData } = this.props;
+this.setState({
+firstName: formData.firstName,
+lastName: formData.lastName,
+address: formData.address,
+city: formData.city,
+state: formData.state,
+zip: formData.zip
+});
+}
+
+_handleChange = event => {
+const { name, value } = event.target;
+this.setState({
+[name]: value
+});
+};
+
+_handleSubmit = event => {
+event.preventDefault();
+const { nextStep, prevStep, setFormData } = this.props;
+const { firstName, lastName, address, city, state, zip } = this.state;
+const formData = {
+firstName: firstName,
+lastName: lastName,
+address: address,
+city: city,
+state: state,
+zip: zip
+};
+setFormData(formData);
+nextStep();
+};
+
+render() {
+const { firstName, lastName, address, city, state, zip } = this.state;
+return (
+<div>
+<h2>Step 2</h2>
+<form onSubmit={this._handleSubmit}>
+<div>
+<label>First Name:</label>
+<input
+           type="text"
+           name="firstName"
+           value={firstName}
+           onChange={this._handleChange}
+         />
+</div>
+<div>
+<label>Last Name:</label>
+<input
+           type="text"
+           name="lastName"
+           value={lastName}
+           onChange={this._handleChange}
+         />
+</div>
+<div>
+<label>Address:</label>
+<input
+           type="text"
+           name="address"
+           value={address}
+           onChange={this._handleChange}
+         />
+</div>
+<div>
+<label>City:</label>
+<input
+           type="text"
+           name="city"
+           value={city}
+           onChange={this._handleChange}
+         />
+</div>
+<div>
+<label>State:</label>
+<input
+           type="text"
+           name="state"
+           value={state}
+           onChange={this._handleChange}
+         />
+</div>
+<div>
+<label>Zip Code:</label>
+<input
+           type="text"
+           name="zip"
+           value={zip}
+           onChange={this._handleChange}
+         />
+</div>
+<button onClick={this.props.prevStep}>Back</button>
+<button type="submit">Next</button>
+</form>
+</div>
+);
+}
+}
+
+const mapStateToProps = state => {
+return {
+formData: state.formData
+};
+};
+
+const mapDispatchToProps = dispatch => {
+return bindActionCreators({
+setFormData
+}, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StepTwo);
