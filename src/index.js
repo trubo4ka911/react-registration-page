de4a93d5e8d5f7126a5cda1a23c1c34ea258fe7e
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-18 02:40:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { createRoot } from "react-dom";
 import rootReducer from "./redux/reducers";
 import MultiStepForm from "./component/MultiStepForm";
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <MultiStepForm />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
