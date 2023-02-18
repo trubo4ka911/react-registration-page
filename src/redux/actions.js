01d9295f4cd409975174328e642defc3c85d602b
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-18 22:44:05 +0200

    Update actions.js

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,46 @@
 export const SET_DETAILS = "SET_DETAILS";
-export const SET_STEP = "SET_STEP";
 export const SET_ERRORS = "SET_ERRORS";
+export const SET_STEP = "SET_STEP";
 export const SUBMIT_FORM = "SUBMIT_FORM";
+export const SET_ADDRESS = "SET_ADDRESS";
+
+export const setDetails = (field, value) => ({
+  type: SET_DETAILS,
+  payload: {
+    field,
+    value,
+  },
+});
+
+export const setErrors = (errors) => ({
+  type: SET_ERRORS,
+  payload: {
+    errors,
+  },
+});
+
+export const nextStep = () => ({
+  type: SET_STEP,
+  payload: {
+    step: 2,
+  },
+});
+
+export const prevStep = () => ({
+  type: SET_STEP,
+  payload: {
+    step: 1,
+  },
+});
+
+export const submitForm = () => ({
+  type: SUBMIT_FORM,
+});
+
+export const setAddress = (field, value) => ({
+  type: SET_ADDRESS,
+  payload: {
+    field,
+    value,
+  },
+});
