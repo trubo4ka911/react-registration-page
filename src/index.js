46a1ac35de609514da3d5f61ed5065e20e9cfc8f
Anna Trubnikova <trubo4ka911@gmail.com>
2023-02-18 02:10:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-import App from './App';
+// import App from './App';
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import MultiStepForm from "./component/MultiStepForm";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MultiStepForm />
+  </Provider>,
+  document.getElementById("root")
 );
